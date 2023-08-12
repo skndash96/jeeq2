1b68dd1d55a52fa34e5e5bd0fe5a10cbf4cd5127
skndash96 <dashskndash@gmail.com>
2023-08-12 22:01:24 +0530

    all data set

--- src/main.py
+++ src/main.py
@@ -1,21 +1,2 @@
-from PIL import Image, ImageEnhance
-from os import listdir
-
-def watermark(code):
-  paths = listdir(f"coll/pics{code}")
-  print(paths)
-  
-  for path in paths:
-    path = f'coll/pics{code}/{path}'
-    i = Image.open(path).convert('RGBA')
-    
-    i = ImageEnhance.Brightness(i).enhance(1.1)
-    i.save(path)
-    
-    i = Image.open(path)
-    i = ImageEnhance.Contrast(i).enhance(5)
-    
-    i.save(path)
-    print("done", path)
-
-watermark("12")
+def main():
+  pass
